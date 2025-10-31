522a43c75bb421959a680b35cd87f8a7902870e0
majid-200 <a.yahia@esi-sba.dz>
2025-10-31 22:57:40 +0100

    Reorganizing and adding Godot first game

---
+++ Godot/first-game/scripts/game_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var score = 0
+
+@onready var score_label: Label = $ScoreLabel
+
+func add_point():
+	score += 1
+	score_label.text = "Congrats, you collected " + str(score) + " coins."
---
+++ Godot/first-game/scripts/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true 
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+		
+	# Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
